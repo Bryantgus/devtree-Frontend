cda0a4cd75d468809a82bcd836930c53eba8e8f9
Bryant <bryanttejeda628f@hotmail.com>
2025-03-28 17:05:45 -0400

    auth user page

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom"
 import { useForm} from "react-hook-form"
 import ErrorMessage from "../components/ErrorMessage";
 import type { RegisterForm } from "../types";
-import axios, { isAxiosError } from "axios";
+import { isAxiosError } from "axios";
 import { toast } from "sonner";
+import api from "../config/axios";
 
 export default function RegisterView() {
 
@@ -22,11 +23,8 @@ export default function RegisterView() {
     
     const handleRegister = async (formData: RegisterForm) => {
         try {
-            const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, formData)
-            console.log(data);
+            const { data } = await api.post('/auth/register', formData)
             toast.success(data)
-            
-            
         } catch (error) {
             if(isAxiosError(error) && error.response) {
                 console.log(error.response.data.error)
